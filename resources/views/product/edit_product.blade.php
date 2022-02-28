d8d318ad715a2a58ab1c7bcfe111f8824469a230
atif-masood-accrual <atif.masood@accrualhub.com>
2022-02-28 13:13:25 +0500

    kindly merge

--- resources/views/product/edit_product.blade.php
+++ resources/views/product/edit_product.blade.php
@@ -51,13 +51,44 @@ Edit Product
 					                                </div>
 					                            </div>
 
-					                            <div class="form-group">
-					                                <label>Product Price</label>
-					                                <div>
-					                                    <input class="form-control" type="number" name="price" id="price" value="{{ $product->price }}" placeholder="Enter Price">
-					                                    <div style="color:red;">{{$errors->first('price')}}</div> <br>
-					                                    
-					                                </div>
+					                            <div class="row">
+						                            <div class="col-6">
+						                                <label>Product Orignal Price From</label>
+						                                <div>
+						                                    <input class="form-control" type="number" name="old_from_price" id="old_from_price" placeholder="Enter Orignal Price From" value="{{ $product->old_from_price }}">
+						                                    <div style="color:red;">{{$errors->first('old_from_price')}}</div> <br>
+						                                    
+						                                </div>
+						                            </div>
+					                            
+						                            <div class="col-6">
+						                                <label>Product Orignal Price to</label>
+						                                <div>
+						                                    <input class="form-control" type="number" name="old_to_price" id="old_to_price" placeholder="Enter Orignal Price To" value="{{ $product->old_to_price }}">
+						                                    <div style="color:red;">{{$errors->first('old_to_price')}}</div> <br>
+						                                    
+						                                </div>
+						                            </div>
+						                        </div>
+						                        <div class="row">
+						                            <div class="col-6">
+						                                <label>Product Discount Price From</label>
+						                                <div>
+						                                    <input class="form-control" type="number" name="new_from_price" id="new_from_price" placeholder="Enter Discount Price From" value="{{ $product->new_from_price }}">
+						                                    <div style="color:red;">{{$errors->first('new_from_price')}}</div> <br>
+						                                    
+						                                </div>
+						                            </div>
+						                        
+
+						                            <div class="col-6">
+						                                <label>Product Discount Price To</label>
+						                                <div>
+						                                    <input class="form-control" type="number" name="new_to_price" id="new_to_price" placeholder="Enter Discount Price To" value="{{ $product->new_to_price }}">
+						                                    <div style="color:red;">{{$errors->first('new_to_price')}}</div> <br>
+						                                    
+						                                </div>
+						                            </div>
 					                            </div>
 
 					                            <div class="uploadDiv" style="padding-left: 10px;">
