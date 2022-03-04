001385de575473ae9b2bb7f3067f339c5b624442
atif-masood-accrual <atif.masood@accrualhub.com>
2022-03-04 16:32:43 +0500

    sms changes 3

--- app/Http/Controllers/TwilioSMSController.php
+++ app/Http/Controllers/TwilioSMSController.php
@@ -15,18 +15,23 @@ class TwilioSMSController extends Controller
         $message = "This is testing from Acuralhub.com from rawalpindi";
   
         try {
-
-            $account_sid = env("AC7fe22859b60777bf2348bd95c4f6958f");
-            $auth_token = env("7e93da03164080ec5634b01d48aae213");
-            $twilio_number = env("+972533856105");
-            
-  
-            $client = new Client($account_sid, $auth_token);
-            $client->messages->create($receiverNumber, [
-                'from' => $twilio_number, 
-                'body' => $message]);
-  
-            dd('SMS Sent Successfully.');
+            //  sms sending
+            $sid = env("TWILIO_SID");
+            return $sid;
+          
+            $clientNumber = Customer::where('id', $id)->value('phone');
+            // comment twillio not working===========obaid
+            $token = env("TWILIO_AUTH_TOKEN");
+            return $token;
+            $twilio = new Client($sid, $token);
+            $message = $twilio->messages
+                ->create(
+                    "+1$clientNumber", // to
+                    array(
+                        "from" => "+16472497970",
+                        "body" => "Hi " . $customerName . ". The payment you sent to EmpireOne Credit Solutions for the amount of  $ " . number_format((float)$amount, 2, '.', '') . " has been accepted. Your balance now is $ $amount_pending."
+                    )
+                );
   
         } catch (Exception $e) {
             dd("Error: ". $e->getMessage());
--- config/services.php
+++ config/services.php
@@ -32,8 +32,7 @@ return [
 
     'twilio' => [
       'sid' => env('TWILIO_SID'),
-      'token' => env('TWILIO_TOKEN'),
-      'from' => env('TWILIO_FROM')
+      'token' => env('TWILIO_AUTH_TOKEN'),
     ],
 
 ];
