7ac0b7a99ce36001a20a6b7976af6bb031ba32d3
atif-masood-accrual <atif.masood@accrualhub.com>
2022-03-04 17:34:24 +0500

    sms changes 4

--- app/Http/Controllers/TwilioSMSController.php
+++ app/Http/Controllers/TwilioSMSController.php
@@ -11,27 +11,22 @@ class TwilioSMSController extends Controller
 {
     public function index()
     {
-        $receiverNumber = "+923075207206";
+        $receiverNumber = "+972533856105";
         $message = "This is testing from Acuralhub.com from rawalpindi";
   
         try {
-            //  sms sending
-            $sid = env("TWILIO_SID");
-            return $sid;
-          
-            $clientNumber = Customer::where('id', $id)->value('phone');
-            // comment twillio not working===========obaid
-            $token = env("TWILIO_AUTH_TOKEN");
-            return $token;
-            $twilio = new Client($sid, $token);
-            $message = $twilio->messages
-                ->create(
-                    "+1$clientNumber", // to
-                    array(
-                        "from" => "+16472497970",
-                        "body" => "Hi " . $customerName . ". The payment you sent to EmpireOne Credit Solutions for the amount of  $ " . number_format((float)$amount, 2, '.', '') . " has been accepted. Your balance now is $ $amount_pending."
-                    )
-                );
+
+            $account_sid = 'AC7fe22859b60777bf2348bd95c4f6958f';
+            $auth_token = '914c00291349b6ee07c75b6da728f00f';
+            $twilio_number = '+13213365311';
+
+  
+            $client = new Client($account_sid, $auth_token);
+            $client->messages->create($receiverNumber, [
+                'from' => $twilio_number, 
+                'body' => $message]);
+  
+            dd('SMS Sent Successfully.');
   
         } catch (Exception $e) {
             dd("Error: ". $e->getMessage());
