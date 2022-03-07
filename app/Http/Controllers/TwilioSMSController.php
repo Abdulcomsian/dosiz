081bb3e73fedc93036a9487a3e0b53e663c3ce30
atif-masood-accrual <atif.masood@accrualhub.com>
2022-03-07 20:17:47 +0500

    sms testing

--- app/Http/Controllers/TwilioSMSController.php
+++ app/Http/Controllers/TwilioSMSController.php
@@ -18,31 +18,30 @@ class TwilioSMSController extends Controller
         // dd($request->all());
         $subscriber_details = Subscriber::where('subscriber_list_id',$request->subscriber_list_id)->get();
         // dd($subscriber_detail);
-        foreach($subscriber_details as $subscriber_detail){
-
-            $receiverNumber = $subscriber_detail->phone;
-            $message = $request->description;
+        
       
             try {
+                foreach($subscriber_details as $subscriber_detail){
 
-                $account_sid = 'AC7fe22859b60777bf2348bd95c4f6958f';
-                $auth_token = 'fa640001b75ae92890d57e7159b2c443';
-                $twilio_number = '+13213365311';
-                
+                    $receiverNumber = $subscriber_detail->phone;
+                    $message = $request->description;
 
-      
-                $client = new Client($account_sid, $auth_token);
-                $client->messages->create($receiverNumber, [
-                    'from' => $twilio_number, 
-                    'body' => $message]);
-      
-                // return Redirect::back();
-                toastSuccess('Successfully Update');
-                return redirect('dashboard/subscribe');
-      
+                    $account_sid = 'AC7fe22859b60777bf2348bd95c4f6958f';
+                    $auth_token = 'fa640001b75ae92890d57e7159b2c443';
+                    $twilio_number = '+13213365311';
+                    
+                    $client = new Client($account_sid, $auth_token);
+                    $client->messages->create($receiverNumber, [
+                        'from' => $twilio_number, 
+                        'body' => $message]);
+          
+                    // return Redirect::back();
+                    toastSuccess('Successfully Update');
+                    return redirect('dashboard/subscribe');
+                }
+
             } catch (Exception $e) {
                 dd("Error: ". $e->getMessage());
             }
-        }
     }
 }
